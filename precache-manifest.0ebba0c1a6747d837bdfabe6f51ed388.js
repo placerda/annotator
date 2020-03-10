2efc64879dc832bb5e1515a79cb263d1b0d3cf51
Paulo Lacerda <pclacerda@gmail.com>
2020-03-10 15:19:12 -0300

    Updates

---
+++ precache-manifest.0ebba0c1a6747d837bdfabe6f51ed388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507357ede964b1d0827854c0552c0cac",
+    "url": "/annotator/index.html"
+  },
+  {
+    "revision": "307b44bda088770ceae7",
+    "url": "/annotator/static/css/main.f3a301ef.chunk.css"
+  },
+  {
+    "revision": "35880159e10f54ac2924",
+    "url": "/annotator/static/js/2.09506f65.chunk.js"
+  },
+  {
+    "revision": "97904f26721bd34df111b552a1daeb32",
+    "url": "/annotator/static/js/2.09506f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307b44bda088770ceae7",
+    "url": "/annotator/static/js/main.7efa09a3.chunk.js"
+  },
+  {
+    "revision": "4b85a79bd332b4886076",
+    "url": "/annotator/static/js/runtime-main.0a148955.js"
+  }
+]);
